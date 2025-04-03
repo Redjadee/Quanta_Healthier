45e13ad7f8c9438f3343aeb0de70eb9d9dd4258f
Redjade <jadered06@outlook.com>
2025-04-03 17:13:10 +0800

    取消了各个tab页面的头部

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,26 +9,26 @@ export default function TabLayout() {
                 tabBarStyle: { //底部样式
                     backgroundColor: 'black'
                 },
-
-                headerStyle: { //头部样式
-                    backgroundColor: 'black'
-                },
-                headerShadowVisible: false, //取消头部阴影
-                headerTintColor: 'white' //当前头部标题颜色
             }}>
+
             <Tabs.Screen name="index" options={{
+                headerShown: false,
                 title: '日历',//显示文本
                 tabBarIcon: ({color, focused}) => ( //图标
                     <Ionicons name={focused? 'home-sharp' : 'home-outline'} color={color} size={24}></Ionicons>
                 )
             }}></Tabs.Screen>
+            
             <Tabs.Screen name="community" options={{
+                headerShown: false,
                 title: '互动',
                 tabBarIcon: ({color, focused}) => (
                     <Ionicons name={focused? 'information-circle' : 'information-circle-outline'} color={color} size={24} />
                 )
             }}></Tabs.Screen>
+            
             <Tabs.Screen name="my" options={{
+                headerShown: false,
                 title: '我的',
                 tabBarIcon: ({color, focused}) => (
                     <Ionicons name={focused? 'information-circle' : 'information-circle-outline'} color={color} size={24} />
